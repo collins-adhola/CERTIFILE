cdcda9fe8c0fac6a964efcafeb2113dd74d21af3
Collins Adhola <coadhola@gmail.com>
2025-09-19 20:54:16 +0100

    Pages

--- src/app/pages/how/how.page.ts
+++ src/app/pages/how/how.page.ts
@@ -1,28 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {
-  IonContent,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardContent,
-} from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-how',
   templateUrl: './how.page.html',
   styleUrls: ['./how.page.scss'],
   standalone: true,
-  imports: [
-    IonContent,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardContent,
-    CommonModule,
-    FormsModule,
-  ],
+  imports: [IonContent, CommonModule, FormsModule],
 })
 export class HowPage implements OnInit {
   constructor() {}
